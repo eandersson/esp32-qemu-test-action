5f1e019d9202f99214bf21707c5cdf8fb0037dec
Erik Olof Gunnar Andersson <me@eandersson.net>
2024-12-06 21:48:38 +0100

    Fix QEMU

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 set -e
 CODE_PATH="$1"
 . $IDF_PATH/export.sh
-cd build 
+cd ${CODE_PATH}
+cd build
 esptool.py --chip esp32s3 merge_bin --fill-flash-size 16MB -o flash_image.bin @flash_args
 /opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw -serial file:output.log
 grep -q -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log && ruby /opt/Unity-*/auto/parse_output.rb -xml output.log || exit 1
