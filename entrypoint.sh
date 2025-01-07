d4610b5aa2804ca7edbf222d3d009e44ee7cd400
Erik Olof Gunnar Andersson <eandersson@users.noreply.github.com>
2025-01-07 15:13:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,14 @@ CODE_PATH="$1"
 . $IDF_PATH/export.sh
 cd ${CODE_PATH}
 cd build
+
+echo "Generating binary"
 esptool.py --chip esp32s3 merge_bin --fill-flash-size 16MB -o flash_image.bin @flash_args
 
+echo "Running virtual machine"
 /opt/qemu/bin/qemu-system-xtensa -machine esp32s3 -nographic -no-reboot -drive file=flash_image.bin,if=mtd,format=raw -m 4 -serial file:output.log  || echo "error"
+
+echo "Generate report"
 cat output.log
 grep -q -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log && ruby /opt/Unity-*/auto/parse_output.rb -xml output.log || exit 1
 mv report.xml /github/workspace
