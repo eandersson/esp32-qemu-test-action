a7c890fb0009d1da7e1ecd73366b5c6107d2a78a
Erik Olof Gunnar Andersson <eandersson@users.noreply.github.com>
2025-01-07 14:49:34 +0100

    Update entrypoint.sh2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,6 @@ cd build
 esptool.py --chip esp32s3 merge_bin --fill-flash-size 16MB -o flash_image.bin @flash_args
 
 /opt/qemu/bin/qemu-system-xtensa -machine esp32s3 -nographic -no-reboot -drive file=flash_image.bin,if=mtd,format=raw -m 4 -serial file:output.log
+cat output.log
 grep -q -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log && ruby /opt/Unity-*/auto/parse_output.rb -xml output.log || exit 1
 mv report.xml /github/workspace
